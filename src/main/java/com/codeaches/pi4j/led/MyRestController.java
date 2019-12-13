5fb87c21d2936b02eafc6b5373bba3351f47bb84
codeaches <info001@codeaches.com>
2019-12-13 05:49:42 -0700

    Raspberry PI

--- src/main/java/com/codeaches/pi4j/led/MyRestController.java
+++ src/main/java/com/codeaches/pi4j/led/MyRestController.java
@@ -1,5 +1,7 @@
 package com.codeaches.pi4j.led;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,21 @@ import com.pi4j.io.gpio.GpioPinDigitalOutput;
 @RestController
 public class MyRestController {
 
+  Logger log = LoggerFactory.getLogger(MyRestController.class);
+
   @Autowired
   @Qualifier("pin01")
   GpioPinDigitalOutput pin01;
 
   @GetMapping("/toggleLED")
   public void toggleLED() {
+
+    String state = pin01.getState().isHigh() ? "ON" : "OFF";
+    log.info("PIN01 state before toggle: " + state);
+
     pin01.toggle();
+
+    state = pin01.getState().isHigh() ? "ON" : "OFF";
+    log.info("PIN01 state after toggle: " + state);
   }
 }
