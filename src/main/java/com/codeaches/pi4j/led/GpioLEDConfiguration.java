3a7d3852172e8f72f694346006b38d42bec874bd
codeaches <info001@codeaches.com>
2019-12-14 06:31:21 -0700

    Raspberry PI

---
+++ src/main/java/com/codeaches/pi4j/led/GpioLEDConfiguration.java
@@ -0,0 +1,27 @@
+package com.codeaches.pi4j.led;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+@Configuration
+public class GpioLEDConfiguration {
+  Logger log = LoggerFactory.getLogger(GpioLEDConfiguration.class);
+
+  @Bean
+  public GpioController gpioController() {
+    return GpioFactory.getInstance();
+  }
+
+  @Bean("pin01")
+  GpioPinDigitalOutput pin01() {
+    return gpioController().provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+  }
+}
