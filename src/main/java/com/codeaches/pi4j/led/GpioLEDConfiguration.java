714e1c10a32f3e874229159ca347c1f165cf5d56
codeaches <info001@codeaches.com>
2019-12-14 10:53:09 -0700

    Raspberry PI

--- src/main/java/com/codeaches/pi4j/led/GpioLEDConfiguration.java
+++ src/main/java/com/codeaches/pi4j/led/GpioLEDConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
@@ -19,16 +20,30 @@ public class GpioLEDConfiguration {
 
   @Bean
   public GpioController gpioController() {
+
+    // create gpio controller
     return GpioFactory.getInstance();
   }
 
   @Bean("pin01")
   GpioPinDigitalOutput pin01() {
-    return gpioController().provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+
+    // provision gpio pin #01 as an output pin and turn off
+    GpioPinDigitalOutput pin01 = gpioController().provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
+
+    // configure the pin shutdown behavior; these settings will be
+    // automatically applied to the pin when the application is terminated
+    pin01.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
+
+    return pin01;
   }
 
   @PreDestroy
   void preDestroy() {
+
+    // stop all GPIO activity/threads by shutting down the GPIO controller
+    // (this method will forcefully shutdown all GPIO monitoring threads and
+    // scheduled tasks)
 
     if (!gpioController().isShutdown()) {
       gpioController().shutdown();
