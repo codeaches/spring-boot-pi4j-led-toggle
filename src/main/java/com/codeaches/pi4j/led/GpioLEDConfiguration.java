d36b39c6a6ed7c4f47b61906ea6c14eb7eee137c
codeaches <info001@codeaches.com>
2019-12-16 20:42:57 -0700

    no message

--- src/main/java/com/codeaches/pi4j/led/GpioLEDConfiguration.java
+++ src/main/java/com/codeaches/pi4j/led/GpioLEDConfiguration.java
@@ -13,7 +13,6 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
 
 @Configuration
 public class GpioLEDConfiguration {
@@ -31,7 +30,7 @@ public class GpioLEDConfiguration {
 
   @Bean("pin")
   GpioPinDigitalOutput pin() {
-    return gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_03, PinState.LOW);
+    return gpioController.provisionDigitalOutputPin(PiPin.GPIO_02, PinState.LOW);
   }
 
   @PreDestroy
---
+++ src/main/java/com/codeaches/pi4j/led/PiPin.java
@@ -0,0 +1,12 @@
+package com.codeaches.pi4j.led;
+
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class PiPin {
+
+  /**
+   * Raspberry pi: Pin number 3 / GPIO 2 / SDA / Pi4J-RaspiPin.GPIO_08
+   */
+  public static final Pin GPIO_02 = RaspiPin.GPIO_08;
+}
